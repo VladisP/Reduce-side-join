119d9164e24a6f321455b084f2c84ef36a38dd17
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-10 22:36:09 +0300

    Scripted auto-commit on change (2019-10-10 22:36:09) by gitwatch.sh

--- src/main/java/lab2/reducer/DelayReducer.java
+++ src/main/java/lab2/reducer/DelayReducer.java
@@ -5,12 +5,13 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class DelayReducer extends Reducer<AirportsIdWritable, Text, Text, Text> {
 
     @Override
     protected void reduce(AirportsIdWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
-        
+        Iterator<>
     }
 }
