9a818f0d21d97b63cf1bffd8a11e107011f5e40f
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-10 19:53:24 +0300

    Scripted auto-commit on change (2019-10-10 19:53:24) by gitwatch.sh

--- src/main/java/lab2/FlightsMapper.java
+++ src/main/java/lab2/FlightsMapper.java
@@ -4,7 +4,12 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class FlightsMapper extends Mapper<LongWritable, Text, AirportsIdWritable, Text> {
 
-
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        
+    }
 }
