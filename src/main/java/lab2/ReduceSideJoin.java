32481287e585033bde468a87b66b6d0e42998c8d
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-10 22:53:44 +0300

    Scripted auto-commit on change (2019-10-10 22:53:44) by gitwatch.sh

--- src/main/java/lab2/ReduceSideJoin.java
+++ src/main/java/lab2/ReduceSideJoin.java
@@ -1,8 +1,19 @@
 package lab2;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+
+import java.io.IOException;
+
 public class ReduceSideJoin {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        Job job = Job.getInstance();
+        job.setJarByClass(ReduceSideJoin.class);
+        job.setJobName("Reduce-side-join lab2");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, );
     }
 }
