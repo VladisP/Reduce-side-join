bf39f41676635ecba287a0df308c40573e83b30d
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-10 22:09:07 +0300

    Scripted auto-commit on change (2019-10-10 22:09:06) by gitwatch.sh

--- src/main/java/lab2/FlightsMapper.java
+++ src/main/java/lab2/FlightsMapper.java
@@ -5,11 +5,21 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 public class FlightsMapper extends Mapper<LongWritable, Text, AirportsIdWritable, Text> {
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        
+
+        FlightTableWritable flightTable = new FlightTableWritable(value);
+
+        Optional.ofNullable(flightTable.getDelayTime()).ifPresent(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                context.write(new AirportsIdWritable(flightTable.getDestAirportId(), 1));
+            }
+        });
     }
 }
