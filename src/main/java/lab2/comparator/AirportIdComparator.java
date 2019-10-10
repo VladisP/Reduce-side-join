4bd4d07da62631fc172798af89d8f150281dea16
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-10 22:26:24 +0300

    Scripted auto-commit on change (2019-10-10 22:26:23) by gitwatch.sh

--- src/main/java/lab2/comparator/AirportIdComparator.java
+++ src/main/java/lab2/comparator/AirportIdComparator.java
@@ -1,4 +1,6 @@
 package lab2.comparator;
 
-public class AirportIdComparator extends Wr {
+import org.apache.hadoop.io.WritableComparator;
+
+public class AirportIdComparator extends WritableComparator {
 }
