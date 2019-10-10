65f0b87019b7119f01912ace136bb51eb53560d1
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-10 22:06:25 +0300

    Scripted auto-commit on change (2019-10-10 22:06:25) by gitwatch.sh

--- src/main/java/lab2/FlightTableWritable.java
+++ src/main/java/lab2/FlightTableWritable.java
@@ -15,21 +15,31 @@ public class FlightTableWritable implements Writable {
     public FlightTableWritable(Text text) {
         String[] columns = text.toString().replaceAll(",", "\"").split(",");
         destAirportId = Integer.parseInt(columns[14].replaceAll("\"", ""));
+
         String potentialDelayTime = columns[17].replaceAll("\"", "");
+
         delayTime = potentialDelayTime.equals("") ? null :
                 Float.parseFloat(potentialDelayTime) > 0 ? null :
                         potentialDelayTime.replaceAll("-", "");
     }
 
+    public int getDestAirportId() {
+        return destAirportId;
+    }
+
+    public String getDelayTime() {
+        return delayTime;
+    }
+
     @Override
     public void write(DataOutput dataOutput) throws IOException {
-        dataOutput.writeInt(airportId);
+        dataOutput.writeInt(destAirportId);
         dataOutput.writeUTF(delayTime);
     }
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
-        airportId = dataInput.readInt();
+        destAirportId = dataInput.readInt();
         delayTime = dataInput.readUTF();
     }
 }
