98b659347ecd52ac2173a58cb46d9d585eb58c61
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-10 22:18:05 +0300

    Scripted auto-commit on change (2019-10-10 22:18:05) by gitwatch.sh

--- src/main/java/lab2/AirportIdPartitioner.java
+++ src/main/java/lab2/AirportIdPartitioner.java
@@ -4,4 +4,8 @@ import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 
 public class AirportIdPartitioner<K,V> extends HashPartitioner<K,V> {
 
+    @Override
+    public int getPartition(K key, V value, int numReduceTasks) {
+        return ;
+    }
 }
