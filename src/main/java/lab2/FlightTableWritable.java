374b192f3a87441dc237ba2aeb7eff1a4cd998ec
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-10 21:53:15 +0300

    Scripted auto-commit on change (2019-10-10 21:53:15) by gitwatch.sh

--- src/main/java/lab2/FlightTableWritable.java
+++ src/main/java/lab2/FlightTableWritable.java
@@ -1,5 +1,6 @@
 package lab2;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 
 import java.io.DataInput;
@@ -11,13 +12,29 @@ public class FlightTableWritable implements Writable {
     private int destAirportId;
     private String airportName;
 
+    public AirportTableWritable(Text text) {
+        String[] columns = text.toString().replaceAll("\"", "").split(",");
+        airportId = Integer.parseInt(columns[0]);
+        airportName = columns[1];
+    }
+
+    public int getAirportId() {
+        return airportId;
+    }
+
+    public String getAirportName() {
+        return airportName;
+    }
+
     @Override
     public void write(DataOutput dataOutput) throws IOException {
-
+        dataOutput.writeInt(airportId);
+        dataOutput.writeUTF(airportName);
     }
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
-
+        airportId = dataInput.readInt();
+        airportName = dataInput.readUTF();
     }
 }
