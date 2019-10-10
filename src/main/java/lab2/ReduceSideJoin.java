c8718696a0a98b50081bee84acff519e6545412b
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-10 23:01:18 +0300

    Scripted auto-commit on change (2019-10-10 23:01:18) by gitwatch.sh

--- src/main/java/lab2/ReduceSideJoin.java
+++ src/main/java/lab2/ReduceSideJoin.java
@@ -1,19 +1,42 @@
 package lab2;
 
+import lab2.comparator.AirportIdComparator;
+import lab2.mappers.AirportsMapper;
+import lab2.mappers.FlightsMapper;
+import lab2.partitioner.AirportIdPartitioner;
+import lab2.reducer.DelayReducer;
+import lab2.writables.AirportsIdWritable;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
 public class ReduceSideJoin {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         Job job = Job.getInstance();
         job.setJarByClass(ReduceSideJoin.class);
         job.setJobName("Reduce-side-join lab2");
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, );
+
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightsMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportsMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        job.setPartitionerClass(AirportIdPartitioner.class);
+        job.setGroupingComparatorClass(AirportIdComparator.class);
+        job.setReducerClass(DelayReducer.class);
+
+        job.setMapOutputKeyClass(AirportsIdWritable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
