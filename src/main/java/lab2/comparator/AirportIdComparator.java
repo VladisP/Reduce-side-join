73205c6059f91099c83c18239a3660703840c27b
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-10 23:45:46 +0300

    Scripted auto-commit on change (2019-10-10 23:45:46) by gitwatch.sh

--- src/main/java/lab2/comparator/AirportIdComparator.java
+++ src/main/java/lab2/comparator/AirportIdComparator.java
@@ -7,6 +7,11 @@ import org.apache.hadoop.io.WritableComparator;
 public class AirportIdComparator extends WritableComparator {
 
     @Override
+    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+        return super.compare(b1, s1, l1, b2, s2, l2);
+    }
+
+    @Override
     public int compare(WritableComparable a, WritableComparable b) {
         int firstAirportId = ((AirportsIdWritable) a).getAirportId();
         int secondAirportId = ((AirportsIdWritable) b).getAirportId();
