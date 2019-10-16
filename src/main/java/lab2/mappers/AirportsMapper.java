517b065bca0d5a776c6a9b7819ec45382a2f60e2
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-16 21:23:18 +0300

    Scripted auto-commit on change (2019-10-16 21:23:18) by gitwatch.sh

--- src/main/java/lab2/mappers/AirportsMapper.java
+++ src/main/java/lab2/mappers/AirportsMapper.java
@@ -1,7 +1,6 @@
 package lab2.mappers;
 
 import lab2.CsvParser;
-import lab2.writables.AirportTableWritable;
 import lab2.writables.AirportsIdWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -11,22 +10,33 @@ import java.io.IOException;
 
 public class AirportsMapper extends Mapper<LongWritable, Text, AirportsIdWritable, Text> {
 
+    private static final int AIRPORT_ID_COLUMN = 0;
+    private static final int AIRPORT_NAME_COLUMN = 1;
+    private static final String FIRST_COLUMN_NAME = "Code";
+
+    private int getAirportId(String[] columns) {
+        String potentialAirportId = columns[AIRPORT_ID_COLUMN];
+
+        return potentialAirportId.equals(FIRST_COLUMN_NAME) ? -1 : Integer.parseInt(potentialAirportId);
+    }
+
+    private String getAirportName(String[] columns) {
+
+        return columns.length == 3 ?
+                columns[AIRPORT_NAME_COLUMN] + columns[AIRPORT_NAME_COLUMN + 1] :
+                columns[AIRPORT_NAME_COLUMN];
+    }
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
-        String[] columns = CsvParser.getColumns(text);
-        String potentialAirportId = columns[AIRPORT_ID_COLUMN];
+        String[] columns = CsvParser.getColumns(value);
+        int airportId = getAirportId(columns);
 
-        airportId = potentialAirportId.equals("Code") ? -1 : Integer.parseInt(columns[0]);
+        if (airportId != -1) {
 
-        airportName = columns.length == 3 ?
-                columns[AIRPORT_NAME_COLUMN] + columns[AIRPORT_NAME_COLUMN + 1] :
-                columns[AIRPORT_NAME_COLUMN];
-
-        if (airportTable.getAirportId() != -1) {
-
-            context.write(new AirportsIdWritable(airportTable.getAirportId(), 0),
-                    new Text(airportTable.getAirportName()));
+            context.write(new AirportsIdWritable(airportId, 0),
+                    new Text(getAirportName(columns)));
         }
     }
 }
